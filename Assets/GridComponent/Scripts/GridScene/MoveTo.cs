56c0168eff82828e0421be60c5d7012e86ba6f9e
yung lean <33080132+Michael23B@users.noreply.github.com>
2018-10-13 22:27:16 +1000

    Refactored folders, event handler changes

---
+++ Assets/GridComponent/Scripts/GridScene/MoveTo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTo : MonoBehaviour
+{
+    public GameObject thingy;
+
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //https://www.youtube.com/watch?v=oXKyqAltCgI
+}
